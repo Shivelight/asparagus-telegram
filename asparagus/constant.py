1ce597373ad329ebadc52c28d6f9fe57e424896b
Shivelight <cwkfr@protonmail.com>
2019-12-29 17:03:49 +0700

    Update RSS URLs to avoid HTTP 301 redirect

--- asparagus/constant.py
+++ asparagus/constant.py
@@ -1,3 +1,3 @@
 TELEGRAM_API = "https://api.telegram.org/bot{token}/{method}"
-FEED_NEWS = "https://www.archlinux.org/feeds/news"
-FEED_PKG_UPDATE_ALL = "https://www.archlinux.org/feeds/packages"
+FEED_NEWS = "https://www.archlinux.org/feeds/news/"
+FEED_PKG_UPDATE_ALL = "https://www.archlinux.org/feeds/packages/"
